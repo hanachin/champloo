e19e3cd3784ab006589d073c2d34bc0664e15ed1
Seiei Miyagi <hanachin@gmail.com>
2016-07-01 01:38:41 +0900

    Compare #to_binary return value and nbt file content directly

--- test/champloo/nbt/test_named_binary_tag.rb
+++ test/champloo/nbt/test_named_binary_tag.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class Champloo::NBT::NamedBinaryTagTest < Minitest::Test
   def setup
-    @nbt = Champloo::NBT::NamedBinaryTag.read('level.dat.uncompressed')
+    @nbt_content = File.binread('level.dat.uncompressed')
+    @nbt = Champloo::NBT::NamedBinaryTag.new(@nbt_content)
   end
 
   def test_to_binary
-    assert { Champloo::NBT::NamedBinaryTag.new(@nbt.to_binary).data == @nbt.data }
+    assert { @nbt.to_binary == @nbt_content }
   end
 end
