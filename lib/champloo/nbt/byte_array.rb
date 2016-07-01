1d3652db37aa9965748876e68f89c6bcd4601626
Seiei Miyagi <hanachin@gmail.com>
2016-07-02 03:03:18 +0900

    Implement Champloo::NBT::ByteArray#to_binary

--- lib/champloo/nbt/byte_array.rb
+++ lib/champloo/nbt/byte_array.rb
@@ -4,6 +4,11 @@ module Champloo
       def tag_type
         Champloo::NBT::TAG_Byte_Array
       end
+
+      def to_binary
+        length = Champloo::NBT::Int.new(self.length)
+        length.to_binary + map(&:to_binary).join
+      end
     end
   end
 end
